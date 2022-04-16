65703d354ff9f3770ae92f45dcc90bb1f1cc86bc
Lovish Tater <lovishtater@gmail.com>
2022-04-17 04:58:31 +0530

    Added Service Section

--- src/core/Home.js
+++ src/core/Home.js
@@ -60,10 +60,58 @@ export default function Home() {
         </div>
       </section>
       <section class="container py-5">
+        <div class="row text-center pt-5 pb-3">
+          <div class="col-lg-6 m-auto">
+            <h1 class="h1">Our Services</h1>
+            <p>We provide the best services to our customers.</p>
+          </div>
+        </div>
+        <div class="row">
+          <div class="col-md-6 col-lg-3 pb-5">
+            <div class="h-100 py-5 services-icon-wap shadow">
+              <div class="h1 text-info text-center">
+                <i class="fa fa-truck fa-lg"></i>
+              </div>
+              <h2 class="h5 mt-4 text-center">Delivery Services</h2>
+            </div>
+          </div>
+
+          <div class="col-md-6 col-lg-3 pb-5">
+            <div class="h-100 py-5 services-icon-wap shadow">
+              <div class="h1 text-info text-center">
+                <i class="fa fa-exchange"></i>
+              </div>
+              <h2 class="h5 mt-4 text-center">Shipping & Return</h2>
+            </div>
+          </div>
+
+          <div class="col-md-6 col-lg-3 pb-5">
+            <div class="h-100 py-5 services-icon-wap shadow">
+              <div class="h1 text-info text-center">
+                <i class="fa fa-service"></i>
+              </div>
+              <h2 class="h5 mt-4 text-center">Repair</h2>
+            </div>
+          </div>
+
+          <div class="col-md-6 col-lg-3 pb-5">
+            <div class="h-100 py-5 services-icon-wap shadow">
+              <div class="h1 text-info text-center">
+                <i class="fa fa-user"></i>
+              </div>
+              <h2 class="h5 mt-4 text-center">24 Hours Service</h2>
+            </div>
+          </div>
+        </div>
+      </section>
+      <section class="container py-2">
         <div class="row text-center pt-3 pb-3">
           <div class="col-lg-6 m-auto">
             <h1 class="h1">All our products</h1>
-            <p>Shop for Home Appliances, Laptops & Accessories online at Vijay Electronics</p>
+            <p>
+              Shop for Home Appliances, Laptops & Accessories online at Vijay
+              Electronics
+            </p>
           </div>
         </div>
 
@@ -80,6 +128,7 @@ export default function Home() {
           </div>
         </div>
       </section>
+
       {/* </Grid> */}
     </Base>
   );
