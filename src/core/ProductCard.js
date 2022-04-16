70ca5cda8a82cd577af91cafcf42b36961de5f2c
Lovish Tater <lovishtater@gmail.com>
2022-04-17 04:59:34 +0530

    Card Updated

--- src/core/ProductCard.js
+++ src/core/ProductCard.js
@@ -1,29 +1,10 @@
 import React, { useState, useEffect } from "react";
-import ImageHelper from "./helper/ImageHelper";
+// import ImageHelper from "./helper/ImageHelper";
 import { Redirect } from "react-router-dom";
 import { addItemToCart, removeItemFromCart } from "./helper/cartHelper";
 import '../styles.css';
-import cx from 'clsx';
-import { makeStyles } from '@material-ui/core/styles';
-import Card from '@material-ui/core/Card';
-import CardMedia from '@material-ui/core/CardMedia';
-import CardContent from '@material-ui/core/CardContent';
-import TextInfoContent from '@mui-treasury/components/content/textInfo';
-import { useFourThreeCardMediaStyles } from '@mui-treasury/styles/cardMedia/fourThree';
-import { useN04TextInfoContentStyles } from '@mui-treasury/styles/textInfoContent/n04';
-import { useOverShadowStyles } from '@mui-treasury/styles/shadow/over';
+import { API } from "../backend";
 
-const useStyles = makeStyles(() => ({
-  root: {
-    maxWidth: 343,
-    margin: 'auto',
-    borderRadius: 12,
-    padding: 12,
-  },
-  media: {
-    borderRadius: 6,
-  },
-}));
 
 const ProductCard = ({
   product,
@@ -36,10 +17,6 @@ const ProductCard = ({
   const [redirect, setRedirect] = useState(false);
   const [count, setCount] = useState(product.count);
 
-  const cartTitle = product ? product.name : "A photo from pexels";
-  const cartDescrption = product ? product.description : "Default description";
-  const cartPrice = product ? product.price : "DEFAULT";
-
   const addToCart = () => {
     addItemToCart(product, () => setRedirect(true));
     getARedirect(redirect);
@@ -51,42 +28,73 @@ const ProductCard = ({
     }
   };
 
-  const showAddToCart = addtoCart => {
-    return (
-      addtoCart && (
-        <button
-          onClick={addToCart}
-          className="btn btn-block btn-outline-dark mt-2 mb-2"
-        >
-          Add to Cart
-        </button>
-      )
-    );
-  };
 
-  const showRemoveFromCart = removeFromCart => {
-    return (
-      removeFromCart && (
-        <button
-          onClick={() => {
-            removeItemFromCart(product._id);
-            setReload(!reload);
-          }}
-          className="btn btn-block btn-outline-danger mt-2 mb-2"
-        >
-          Remove from cart
-        </button>
-      )
-    );
-  };
-
-  const styles = useStyles();
-  const mediaStyles = useFourThreeCardMediaStyles();
-  const textCardContentStyles = useN04TextInfoContentStyles();
-  const shadowStyles = useOverShadowStyles({ inactive: true });
 
   return (
-  //  <div class="card-columns">
+    <div class="card" style={{ borderRadius: "15px" }}>
+      <div class="bg-image hover-zoom ripple rounded ripple-surface">
+        <img
+          src={
+            product
+              ? `${API}/product/photo/${product._id}`
+              : `https://images.pexels.com/photos/3561339/pexels-photo-3561339.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940`
+          }
+          style={{
+            borderTopLeftRadius: "15px",
+            borderTopRightRadius: "15px",
+            objectFit: "cover",
+          }}
+          class="img-fluid"
+          alt="Laptop"
+        />
+        <a href="#!">
+          <div class="mask"></div>
+        </a>
+      </div>
+      <div class="card-body pb-0">
+        <div class="d-flex justify-content-between">
+          <div>
+            <p>{product.name}</p>
+            <p class="small  font-weight-normal text-wrap" >
+              {product.description.substring(0, 150) + "..."}
+            </p>
+          </div>
+          <div>
+            <p class="small text-muted">Rated 4.0/5</p>
+          </div>
+        </div>
+      </div>
+      <div class="card-body">
+        <div class="d-flex justify-content-between align-items-center pb-2 mb-1">
+          <p>{"$" + product.price}</p>
+          {addtoCart && (
+            <button
+              onClick={addToCart}
+              className="btn btn-block btn-outline-dark mt-2 mb-2"
+            >
+              Add to Cart
+            </button>
+          )}
+          {removeFromCart && (
+            <button
+              onClick={() => {
+                removeItemFromCart(product._id);
+                setReload(!reload);
+              }}
+              className="btn btn-block btn-outline-danger mt-2 mb-2"
+            >
+              Remove from cart
+            </button>
+          )}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ProductCard;
+    //old card
+    //  <div class="card-columns">
 
     // <div className="card text-light bg-info border border-white rounded ">
     //   <div className="card-header lead">{cartTitle}</div>
@@ -103,28 +111,24 @@ const ProductCard = ({
     //     </div>
     //   </div>
     // </div>
-    
-     <Card className={cx(styles.root, shadowStyles.root)}>
-      {/* <CardMedia
-        className={cx(styles.media, mediaStyles.root)}
-        image={
-          'https://images.unsplash.com/photo-1500462918059-b1a0cb512f1d?ixlib=rb-1.2.1&auto=format&fit=crop&w=934&q=80'
-        }
-      /> */}
-      <ImageHelper product={product} />
-      <CardContent>
-        <TextInfoContent
-          classes={textCardContentStyles}
-          overline={`$`+cartPrice}
-          heading={cartTitle.slice(0, 20)}
-          body= {cartDescrption.slice(0, 100)}
-        />
-        <div className="col-12">{showAddToCart(addtoCart)}</div>
-         <div className="col-12">{showRemoveFromCart(removeFromCart)}</div>
-      </CardContent>
-    </Card>
-   
-  );
-};
+    // </div>
 
-export default ProductCard;
+    //  <Card className={cx(styles.root, shadowStyles.root)}>
+    //   {/* <CardMedia
+    //     className={cx(styles.media, mediaStyles.root)}
+    //     image={
+    //       'https://images.unsplash.com/photo-1500462918059-b1a0cb512f1d?ixlib=rb-1.2.1&auto=format&fit=crop&w=934&q=80'
+    //     }
+    //   /> */}
+    //   <ImageHelper product={product} />
+    //   <CardContent>
+    //     <TextInfoContent
+    //       classes={textCardContentStyles}
+    //       overline={`$`+cartPrice}
+    //       heading={cartTitle.slice(0, 20)}
+    //       body= {cartDescrption.slice(0, 100)}
+    //     />
+    //     <div className="col-12">{showAddToCart(addtoCart)}</div>
+    //      <div className="col-12">{showRemoveFromCart(removeFromCart)}</div>
+    //   </CardContent>
+    // </Card>
