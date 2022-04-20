190a1831cdd08172a88047b23cbd3b86cad6a10e
Lovish Tater <lovishtater@gmail.com>
2022-04-20 10:18:51 +0530

    Added FontAwesome configs

--- src/core/home/brands.js
+++ src/core/home/brands.js
@@ -1,5 +1,9 @@
 import React from 'react'
-
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { 
+  faChevronRight,
+  faChevronLeft,
+ } from "@fortawesome/free-solid-svg-icons";
 const Brands = () => {
   return (
     <section class="bg-light py-5">
@@ -16,20 +20,11 @@ const Brands = () => {
             <div class="row d-flex flex-row">
               <div class="col-1 align-self-center">
                 <a class="h1" href="#brand" role="button" data-bs-slide="prev">
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    width="30"
-                    height="30"
-                    fill="currentColor"
-                    class="bi bi-caret-left-fill"
-                    viewBox="0 0 30 30"
-                  >
-                    <path d="m3.86 8.753 5.482 4.796c.646.566 1.658.106 1.658-.753V3.204a1 1 0 0 0-1.659-.753l-5.48 4.796a1 1 0 0 0 0 1.506z" />
-                  </svg>
+                  <FontAwesomeIcon icon={faChevronLeft} size="xs" />
                 </a>
               </div>
 
-              <div class="col">
+              <div class="col-10">
                 <div
                   class="carousel slide carousel-multi-item pt-2 pt-md-0"
                   id="brand"
@@ -39,32 +34,32 @@ const Brands = () => {
                     <div class="carousel-item active">
                       <div class="row">
                         <div class="col-3 p-md-5">
-                            <img
-                              class="img-fluid brand-img"
-                              src="https://i.ibb.co/xqPcR48/dffedw.png"
-                              alt="Brand Logo"
-                            />
+                          <img
+                            class="img-fluid brand-img"
+                            src="https://i.ibb.co/xqPcR48/dffedw.png"
+                            alt="Brand Logo"
+                          />
                         </div>
                         <div class="col-3 p-md-5">
-                            <img
-                              class="img-fluid brand-img"
-                              src="https://i.ibb.co/MkzVRpG/1200px-Samsung-Logo-svg.png"
-                              alt="Brand Logo"
-                            />
+                          <img
+                            class="img-fluid brand-img"
+                            src="https://i.ibb.co/MkzVRpG/1200px-Samsung-Logo-svg.png"
+                            alt="Brand Logo"
+                          />
                         </div>
                         <div class="col-3 p-md-5">
-                            <img
-                              class="img-fluid brand-img"
-                              src="https://i.ibb.co/RydZn0q/lloyd-logo-6-FFE9-A0779-seeklogo-com.png"
-                              alt="Brand Logo"
-                            />
+                          <img
+                            class="img-fluid brand-img"
+                            src="https://i.ibb.co/RydZn0q/lloyd-logo-6-FFE9-A0779-seeklogo-com.png"
+                            alt="Brand Logo"
+                          />
                         </div>
                         <div class="col-3 p-md-5">
-                            <img
-                              class="img-fluid brand-img"
-                              src="https://i.ibb.co/ckTRxSY/unnamed-1-removebg-preview.png"
-                              alt="Brand Logo"
-                            />
+                          <img
+                            class="img-fluid brand-img"
+                            src="https://i.ibb.co/ckTRxSY/unnamed-1-removebg-preview.png"
+                            alt="Brand Logo"
+                          />
                         </div>
                       </div>
                     </div>
@@ -72,64 +67,64 @@ const Brands = () => {
                     <div class="carousel-item">
                       <div class="row">
                         <div class="col-3 p-md-5">
-                            <img
-                              class="img-fluid brand-img"
-                              src="https://i.ibb.co/748Vgxc/PNGPIX-COM-Whirlpool-Logo-PNG-Transparent.png"
-                              alt="Brand Logo"
-                            />
+                          <img
+                            class="img-fluid brand-img"
+                            src="https://i.ibb.co/748Vgxc/PNGPIX-COM-Whirlpool-Logo-PNG-Transparent.png"
+                            alt="Brand Logo"
+                          />
                         </div>
                         <div class="col-3 p-md-5">
-                            <img
-                              class="img-fluid brand-img"
-                              src="https://i.ibb.co/ckgnywn/png-clipart-logo-hitachi-high-technologies-america-manufacturing-others-electronics-text-removebg-pr.png"
-                              alt="Brand Logo"
-                            />
+                          <img
+                            class="img-fluid brand-img"
+                            src="https://i.ibb.co/ckgnywn/png-clipart-logo-hitachi-high-technologies-america-manufacturing-others-electronics-text-removebg-pr.png"
+                            alt="Brand Logo"
+                          />
                         </div>
                         <div class="col-3 p-md-5">
-                            <img
-                              class="img-fluid brand-img"
-                              src="https://i.ibb.co/qYkcXXp/unnamed-2.png"
-                              alt="Brand Logo"
-                            />
+                          <img
+                            class="img-fluid brand-img"
+                            src="https://i.ibb.co/qYkcXXp/unnamed-2.png"
+                            alt="Brand Logo"
+                          />
                         </div>
                         <div class="col-3 p-md-5">
-                            <img
-                              class="img-fluid brand-img"
-                              src="https://i.ibb.co/0ZBSS6X/175-1755101-o-general-hd-png-download-removebg-preview.png"
-                              alt="Brand Logo"
-                            />
+                          <img
+                            class="img-fluid brand-img"
+                            src="https://i.ibb.co/0ZBSS6X/175-1755101-o-general-hd-png-download-removebg-preview.png"
+                            alt="Brand Logo"
+                          />
                         </div>
                       </div>
                     </div>
                     <div class="carousel-item">
                       <div class="row">
                         <div class="col-3 p-md-5">
-                            <img
-                              class="img-fluid brand-img"
-                              src="https://i.ibb.co/MVDTFd3/ifb-logo-CDA98-EC536-seeklogo-com.png"
-                              alt="Brand Logo"
-                            />
+                          <img
+                            class="img-fluid brand-img"
+                            src="https://i.ibb.co/MVDTFd3/ifb-logo-CDA98-EC536-seeklogo-com.png"
+                            alt="Brand Logo"
+                          />
                         </div>
                         <div class="col-3 p-md-5">
-                            <img
-                              class="img-fluid brand-img"
-                              src="https://i.ibb.co/xqPcR48/dffedw.png"
-                              alt="Brand Logo"
-                            />
+                          <img
+                            class="img-fluid brand-img"
+                            src="https://i.ibb.co/xqPcR48/dffedw.png"
+                            alt="Brand Logo"
+                          />
                         </div>
                         <div class="col-3 p-md-5">
-                            <img
-                              class="img-fluid brand-img"
-                              src="https://i.ibb.co/ckgnywn/png-clipart-logo-hitachi-high-technologies-america-manufacturing-others-electronics-text-removebg-pr.png"
-                              alt="Brand Logo"
-                            />
+                          <img
+                            class="img-fluid brand-img"
+                            src="https://i.ibb.co/ckgnywn/png-clipart-logo-hitachi-high-technologies-america-manufacturing-others-electronics-text-removebg-pr.png"
+                            alt="Brand Logo"
+                          />
                         </div>
                         <div class="col-3 p-md-5">
-                            <img
-                              class="img-fluid brand-img"
-                              src="https://i.ibb.co/qYkcXXp/unnamed-2.png"
-                              alt="Brand Logo"
-                            />
+                          <img
+                            class="img-fluid brand-img"
+                            src="https://i.ibb.co/qYkcXXp/unnamed-2.png"
+                            alt="Brand Logo"
+                          />
                         </div>
                       </div>
                     </div>
@@ -139,16 +134,7 @@ const Brands = () => {
 
               <div class="col-1 align-self-center">
                 <a class="h1" href="#brand" role="button" data-bs-slide="next">
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    width="30"
-                    height="30"
-                    fill="currentColor"
-                    class="bi bi-caret-right-fill"
-                    viewBox="0 0 30 30"
-                  >
-                    <path d="m12.14 8.753-5.482 4.796c-.646.566-1.658.106-1.658-.753V3.204a1 1 0 0 1 1.659-.753l5.48 4.796a1 1 0 0 1 0 1.506z" />
-                  </svg>
+                  <FontAwesomeIcon icon={faChevronRight} size="xs" />
                 </a>
               </div>
             </div>
